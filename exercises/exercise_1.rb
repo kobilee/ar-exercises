b5cd0afb195bc59a3ab8c6cc98f035525bb49b7d
Kobi Lee <kobiatlaslee@gmail.com>
2019-05-14 16:08:58 +0000

    exercise1 and 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Store.create name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true
+Store.create name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false
+Store.create name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true
+
+puts Store.count
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store3 = Store.find_by(id: 3)
+pp @store1
+pp @store2
+pp @store3
+@store1.update(annual_revenue: 224001)
+@store1.save
+pp@store1
