099d5caf31a2d69cd45d8c4e1b3744ece0da196b
Kobi Lee <kobiatlaslee@gmail.com>
2019-05-14 18:19:43 +0000

    exercise 7

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,5 +11,7 @@ Store.create name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens
 Store.create name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false
 Store.create name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true
 
-@mens_stores = Store.where("mens_apparel = ?", true)
-pp @mens_stores
+
+Store.where(mens_apparel: true).find_each do |store,annual_revenue|
+    puts "Name = " + store.name, "Annual Revenue: " + store.annual_revenue.to_s
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :have_apparel
+
+
+  def have_apparel
+    if !mens_apparel && !womens_apparel
+      errors[:base] << "store must have apparel"
+    end
+  end
 
 end
